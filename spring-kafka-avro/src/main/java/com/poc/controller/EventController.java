a605117ff943c338e98c0a97f394a153067590af
Pankaj Singh <p7ksingh@gmail.com>
2024-05-01 22:03:59 +0530

    spring-kafka-avro-schema registery

---
+++ spring-kafka-avro/src/main/java/com/poc/controller/EventController.java
@@ -0,0 +1,21 @@
+package com.poc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.poc.dto.Employee;
+import com.poc.producer.KafkaAvroProducer;
+
+@RestController
+public class EventController {
+    @Autowired
+    private KafkaAvroProducer producer;
+
+    @PostMapping("/events")
+    public String sendMessage(@RequestBody Employee employee) {
+        producer.send(employee);
+        return "message published !";
+    }
+}
